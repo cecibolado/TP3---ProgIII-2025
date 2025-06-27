7ae5372dc2d98105ac6f302bf8049618d009e4b7
Cecilia Mercedes Bolado <ceciliaboladodevita@gmail.com>
2025-06-27 20:37:52 -0300

    Cargo ejercicio 4

---
+++ ejercicio4/app.jsx
@@ -0,0 +1,26 @@
+const {useState} = React
+
+const App = () => {
+    const [izquierdoHabilitado, setIzquierdoHabilitado] = useState (false)
+    const [derechoHabilitado, setDerechoHabilitado] = useState (true)
+
+    const toggleIzquierdo = () => {
+        setIzquierdoHabilitado(!izquierdoHabilitado)
+        setDerechoHabilitado(!derechoHabilitado)
+
+    const toggleDerecho = () => {
+        setIzquierdoHabilitado(!izquierdoHabilitado)
+        setDerechoHabilitado(!derechoHabilitado)
+    }
+
+    return (
+        <>
+        <button disabled={izquierdoHabilitado} onClick={toggleIzquierdo}>Izquierdo</button>
+        <button disabled={derechoHabilitado} onClick={toggleDerecho}>Derecho</button>
+        </>
+    )
+    }
+    const contenedor = document.getElementById("root")
+    const root = ReactDOM.createRoot(contenedor)
+    root.render(<App/>)
+}
